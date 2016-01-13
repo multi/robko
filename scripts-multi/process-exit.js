cc52beb81d6303668148b6dfb4eb75d4979add26
Georgi Yonchev <georgi@yonchev.org>
2016-01-13 10:48:08 +0200

    sleep more

--- scripts-multi/process-exit.js
+++ scripts-multi/process-exit.js
@@ -15,24 +15,14 @@
 
 var exec = require('child_process').exec
 
-var touch = exec.bind(this, 'sleep 1 && touch package.json')
-
 module.exports = function (robot) {
 
   robot.respond(/process exit/i, function (msg) {
     msg.send('OK. Bye bye ...')
+    exec('sleep 2 && touch package.json')
     setTimeout(function () {
-      touch()
       process.exit(0)
-    }, 1000)
-  })
-
-  process.on('uncaughtException', function (err) {
-    console.log('uncaughtException', err, err.stack)
-    setTimeout(function () {
-      touch()
-      process.exit(1)
-    }, 1000)
+    }, 100)
   })
 
 }
