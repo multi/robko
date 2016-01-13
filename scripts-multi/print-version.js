f62784dd8342573b662fb777eb4c962a334c4158
Georgi Yonchev <georgi@yonchev.org>
2016-01-13 13:12:39 +0200

    go public (╯°□°)╯
    + `print version` script

---
+++ scripts-multi/print-version.js
@@ -0,0 +1,24 @@
+// Description:
+//   print version from package.json
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   hubot print version - print package.json#version
+//
+// Author:
+//   multi
+
+var package = require('../package')
+
+module.exports = function (robot) {
+
+  robot.respond(/print version/i, function (msg) {
+    msg.send('running version ' + package.version)
+  })
+
+}
