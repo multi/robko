c2d9bde696357d7dd0bd8c86f63c373ef41f1250
Georgi Yonchev <georgi@yonchev.org>
2016-01-22 02:32:51 +0200

    weather script output formatting updates

--- scripts-multi/weather.js
+++ scripts-multi/weather.js
@@ -9,10 +9,10 @@
 //
 // Commands:
 //   hubot what's the weather in <city[, state]> - show weather info for <location>
-//   hubot search for weather location <query> - search for weather location by given <query>
+//   hubot find weather location <query> - search for weather location by given <query>
 //   hubot remember weather location <location> - add weather location to robot brain
 //   hubot forget weather location <location> - remove weather location from robot brain
-//   hubot list known weather locations - list remembered weather locations
+//   hubot list weather locations - list remembered weather locations
 //
 // Author:
 //   multi
@@ -32,7 +32,7 @@ module.exports = function (robot) {
 
   robot.respond(/what's the weather in (.+)$/i, function (msg) {
     var re = new RegExp(
-      msg.match[1].trim().toLowerCase().split(/,| /).filter(function (i) {
+      '^' + msg.match[1].trim().toLowerCase().split(/,| /).filter(function (i) {
         return i !== ''
       }).join('|'),
       'i'
@@ -43,12 +43,12 @@ module.exports = function (robot) {
     })
 
     if (toQuery.length === 0) {
-      msg.send('nothing found? maybe you should try to search first (hint: @' + robot.name + ' search for weather location <query>) and then tell me to remember it.')
+      msg.send('nothing found? maybe you should try to find it first (hint: @' + robot.name + ' find weather location <query>)')
       return
     }
 
     if (toQuery.length > 1) {
-      msg.send('more than one locations found, please use the full form <city, state|country> (eg. Varna, Bulgara)')
+      msg.send('more than one locations found, please use the full form <city, state> (eg. Varna, Bulgara)')
       return
     }
 
@@ -70,39 +70,66 @@ module.exports = function (robot) {
           return
         }
 
-        if (!result) return
-        if (!result.rss) return
-        if (!result.rss.channel) return
-        if (result.rss.channel.length !== 1) return
-        if (!result.rss.channel[0].item) return
+        switch (true) {
+          case !result:
+          case !result.rss:
+          case !result.rss.channel:
+          case result.rss.channel.length !== 1:
+          case !result.rss.channel[0].item:
+            robot.logger.warn('weather response wrong format', require('util').inspect(result, { depth: null }))
+          case result.rss.channel[0].item.length === 0:
+            msg.send('no weather information found.')
+            return
+        }
+
+        var attachments = result.rss.channel[0].item.map(function (item) {
+          switch (true) {
+            case !item.description:
+            case item.description.length !== 1:
+            case !item.pubDate:
+            case item.pubDate.length !== 1:
+            case !item.title:
+            case item.title.length !== 1:
+              robot.logger.warn('weather response wrong format', require('util').inspect(item, { depth: null }))
+              return
+          }
+
+          var text = item.description[0].match(descriptionTextMatcher)
+          if (!text || text.length !== 2) return
+          text = text[1]
+
+          var imgUrl = item.description[0].match(imgMatcher)
+          if (!imgUrl || imgUrl.length !== 2) return
+          imgUrl = imgUrl[1].replace('_31x31', '')
+
+          return {
+            author_name: item.pubDate[0],
+            title: item.title[0],
+            text: text,
+            thumb_url: imgUrl,
+            fallback: item.title[0] + ' :: ' + text,
+          }
+        }).filter(function (item) {
+          return item !== undefined
+        })
+
+        if (robot.adapterName !== 'slack') {
+          msg.send(attachments.map(function (att) {
+            return att.fallback
+          }).join('\n'))
+
+          return
+        }
 
         robot.emit('slack-attachment', {
           channel: msg.message.room,
-          attachments: result.rss.channel[0].item.map(function (item) {
-            if (!item.description) return
-            if (item.description.length !== 1) return
-
-            var text = item.description[0].match(descriptionTextMatcher)
-            if (!text || text.length !== 2) return
-            text = text[1]
-
-            var imgUrl = item.description[0].match(imgMatcher)
-            if (!imgUrl || imgUrl.length !== 2) return
-            imgUrl = imgUrl[1]
-
-            return {
-              title: item.title,
-              text: text,
-              fallback: item.title + ' ' + text,
-              thumb_url: imgUrl,
-            }
-          })
+          attachments: attachments,
         })
       })
     })
   })
 
-  robot.respond(/search for weather location (.+)$/i, function (msg) {
+  robot.respond(/find weather location (.+)$/i, function (msg) {
     msg.http('http://forecastfox.accuweather.com/adcbin/forecastfox/locate_city.asp')
     .query({
       location: msg.match[1].trim(),
@@ -120,32 +147,30 @@ module.exports = function (robot) {
           return
         }
 
-        if (!result) return
-        if (!result.adc_database) return
-        if (!result.adc_database.citylist) return
-        if (result.adc_database.citylist.length !== 1) {
-          msg.send('no matching locations found.')
-          return
-        }
-        if (!result.adc_database.citylist[0].location) return
-        if (result.adc_database.citylist[0].location.length === 0) {
-          msg.send('no matching locations found.')
-          return
+        switch (true) {
+          case !result:
+          case !result.adc_database:
+          case !result.adc_database.citylist:
+          case result.adc_database.citylist.length !== 1:
+          case !result.adc_database.citylist[0].location:
+            robot.logger.warn('locations response wrong format', require('util').inspect(result, { depth: null }))
+          case result.adc_database.citylist[0].location.length === 0:
+            msg.send('no matching locations found.')
+            return
         }
 
         searchCache = result.adc_database.citylist[0].location
 
         msg.send(result.adc_database.citylist[0].location.map(function (location) {
           return [
-            'City: *',
+            '*City:* ',
             location.$.city,
-            '* State: *',
+            ' *State:* ',
             location.$.state,
-            '* Location: *',
+            ' *Location:* ',
             location.$.location,
-            '*'
           ].join('')
-        }).join('\n'))
+        }).join('\n') + '\nnow, i can remeber location from the search results (hint: @' + robot.name + ' remember weather location <location>)')
       })
     })
   })
@@ -156,7 +181,7 @@ module.exports = function (robot) {
     })
 
     if (toRemember.length !== 1) {
-      msg.send('nothing found? maybe you should try to search first (hint: @' + robot.name + ' search for weather location <query>) and then I can remember it.')
+      msg.send('nothing found by the given location? maybe you should try to find it first (hint: @' + robot.name + ' find weather location <query>) and then I can remember it.')
       return
     }
 
@@ -175,34 +200,33 @@ module.exports = function (robot) {
     })
 
     if (toForgetIndex === -1) {
-      msg.send('nothing found? maybe you should try see all known locations first (hint: @' + robot.name + ' list known weather locations) and then use the `Location` to forget it.')
+      msg.send('nothing found? maybe you should try to list all remembered locations first (hint: @' + robot.name + ' list weather locations) and then use the `Location` to forget it.')
       return
     }
 
     var removed = robot.brain.data._weather.splice(toForgetIndex, 1)
     if (removed.length !== 1) {
-      msg.send('nothing removed? maybe you should try see all known locations first (hint: @' + robot.name + ' list known weather locations) and then use the `Location` to forget it.')
+      msg.send('nothing removed? maybe you should try to list all remembered locations first (hint: @' + robot.name + ' list weather locations) and then use the `Location` to forget it.')
       return
     }
 
     msg.send('ok. removed, *' + removed[0].c + ', ' + removed[0].s + '* _' + removed[0].l + '_')
   })
 
-  robot.respond(/list known weather locations$/i, function (msg) {
+  robot.respond(/list weather locations$/i, function (msg) {
     if (robot.brain.data._weather.length === 0) {
-      msg.send('no locations i know. (hint: @' + robot.name + ' search for weather location <query>)')
+      msg.send('no locations i know. (hint: @' + robot.name + ' find weather location <query>)')
       return
     }
 
     msg.send(robot.brain.data._weather.map(function (location) {
       return [
-        'City: *',
+        '*City:* ',
         location.c,
-        '* State: *',
+        ' *State:* ',
         location.s,
-        '* Location: *',
+        ' *Location:* ',
         location.l,
-        '*'
       ].join('')
     }).join('\n'))
   })
