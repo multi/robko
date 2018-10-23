049abef285782b4bf9fabbd3730980aa0acdec76
Georgi Yonchev <georgi@yonchev.org>
2018-10-23 17:06:18 +0300

    experiment with redisgraph

---
+++ scripts-multi/redisgraph.js
@@ -0,0 +1,50 @@
+// Description:
+//   redisgraph hubot interface
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   hubot rg <query> - execute query on redisgraph
+//
+// Author:
+//   multi
+
+var RedisGraph = require('redisgraph.js')
+var GRAPH_NAME = 'hubot_graph'
+
+module.exports = function (robot) {
+
+  var graph = new RedisGraph(GRAPH_NAME)
+
+  robot.respond(/rg (.+)$/i, function (msg) {
+    if (!msg.match[1]) {
+      msg.send('no query?')
+      return
+    }
+
+    graph
+    .query(msg.match[1])
+    .then(function (res) {
+      var message = []
+      while (res.hasNext()) {
+        var record = res.next()
+        record.keys().forEach(function (key) {
+          message.push(`${key}: ${record.getString(key)}`)
+        })
+    	}
+      if (message.length) {
+        msg.send(message.join('\n'))
+      }
+      msg.send(res.getStatistics().queryExecutionTime())
+    })
+    .catch(function (err) {
+      console.error(err)
+      msg.send(err)
+    })
+  })
+
+}
